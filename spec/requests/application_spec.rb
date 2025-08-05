c6685bda1820a18a70db81e3bb254d070c5c46dc
Jose Rafael Camejo <jose20camejo@gmail.com>
2025-08-05 14:13:12 -0300

    initial commit

---
+++ spec/requests/application_spec.rb
@@ -0,0 +1,22 @@
+require 'swagger_helper'
+
+RSpec.describe 'Application', type: :request do
+  path '/' do
+    get('root path') do
+      tags 'Application'
+      produces 'application/json'
+
+      response(200, 'successful') do
+        schema type: :object,
+               properties: {
+                 message: { type: :string }
+               }
+
+        run_test! do |response|
+          data = JSON.parse(response.body)
+          expect(data['message']).to eq('Hello, world!')
+        end
+      end
+    end
+  end
+end
