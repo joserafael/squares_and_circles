fee373d76e1ec7f3fff7c67dc49532f2f1287c9b
Jose Rafael Camejo <jose20camejo@gmail.com>
2025-08-05 19:08:31 -0300

    change default host in swagger

--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -18,7 +18,7 @@ RSpec.configure do |config|
           url: 'http://{defaultHost}',
           variables: {
             defaultHost: {
-              default: 'localhost:3000'
+              default: 'localhost:'
             }
           }
         }
